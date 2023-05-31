f9d00b2c2d942563f89ce99d67a2a0126f699d5e
Curtis <excentriceevee@hotmail.com>
2023-05-31 02:07:40 -0400

    Add validations to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+    validates :username, :email, :password, presence: { message: ->(object, data) do
+        "Hey, #{data[:attribute].downcase} is empty."
+    end }
+    validates :username, length: { in: 2..16 }
+    validates :email, uniqueness: true
 end
